96bc356764b31815ae08c526fdc7e3fa47d02fb1
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-06-05 20:54:03 +0300

    restrict the player of moving beyond the canvas borders

--- src/components/CanvasElement.tsx
+++ src/components/CanvasElement.tsx
@@ -1,166 +1,174 @@
 import { useRef, useEffect } from "react";
 import {
-  detectCircleCollisionOfTwoCircles,
-  drawCircle,
-  drawRectangle,
+	detectCircleCollisionOfTwoCircles,
+	drawCircle,
+	drawRectangle,
 } from "../utilities/CanvasUtils";
 import { CircleType, RectangleType } from "../types/CanvasTypes";
 
 interface IPlayer {
-  color: string;
-  initialRadius: number;
+	color: string;
+	initialRadius: number;
 }
 
 interface IItem {
-  color: string;
-  radius: number;
-  playerRadiusIncrement: number;
-  playerSpeedIncrement: number;
+	color: string;
+	radius: number;
+	playerRadiusIncrement: number;
+	playerSpeedIncrement: number;
 }
 
 interface IProps {
-  player: IPlayer;
-  items: IItem[];
+	player: IPlayer;
+	items: IItem[];
 }
 
 const CanvasElement = ({ player, items }: IProps) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  let context: CanvasRenderingContext2D | null = null;
-  let playerX = 500;
-  let playerY = 500;
-  let playerRadius = player.initialRadius;
-  let playerSpeed = 1;
-  const readyItems = items.map((x) => {
-    return {
-      r: x.radius,
-      color: x.color,
-      speedIncremet: x.playerSpeedIncrement,
-      radiusIncrement: x.playerRadiusIncrement,
-      x: Math.random() * 185 + 100,
-      y: Math.random() * 320 + 160,
-      isCollected: false,
-    };
-  });
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	let context: CanvasRenderingContext2D | null = null;
+	let playerX = 500;
+	let playerY = 500;
+	let playerRadius = player.initialRadius;
+	let playerSpeed = 1;
+	const readyItems = items.map((x) => {
+		return {
+			r: x.radius,
+			color: x.color,
+			speedIncremet: x.playerSpeedIncrement,
+			radiusIncrement: x.playerRadiusIncrement,
+			x: Math.random() * 500 - x.radius,
+			y: Math.random() * 500 - x.radius,
+			isCollected: false,
+		};
+	});
 
-  const checkPlayerItemsCollision = () => {
-    const filteredItems = readyItems.filter((x) => !x.isCollected);
-    filteredItems.forEach((item) => {
-      const collides = detectCircleCollisionOfTwoCircles(
-        {
-          x: playerX,
-          y: playerY,
-          r: playerRadius,
-          color: player.color,
-        },
-        {
-          x: item.x,
-          y: item.y,
-          r: item.r,
-          color: item.color,
-        },
-      );
+	const checkPlayerItemsCollision = () => {
+		const filteredItems = readyItems.filter((x) => !x.isCollected);
+		filteredItems.forEach((item) => {
+			const collides = detectCircleCollisionOfTwoCircles(
+				{
+					x: playerX,
+					y: playerY,
+					r: playerRadius,
+					color: player.color,
+				},
+				{
+					x: item.x,
+					y: item.y,
+					r: item.r,
+					color: item.color,
+				},
+			);
 
-      if (collides) {
-        playerRadius += item.radiusIncrement;
-        playerSpeed += item.speedIncremet;
-        item.isCollected = true;
-        renderGame();
-      }
-    });
-  };
+			if (collides) {
+				playerRadius += item.radiusIncrement;
+				playerSpeed += item.speedIncremet;
+				item.isCollected = true;
+				renderGame();
+			}
+		});
+	};
 
-  const renderGame = () => {
-    // clear the canvas to preserve the slight transparency
-    const canvas = canvasRef.current;
-    context!.clearRect(0, 0, canvas!.width, canvas!.height);
+	const renderGame = () => {
+		// clear the canvas to preserve the slight transparency
+		const canvas = canvasRef.current;
+		context!.clearRect(0, 0, canvas!.width, canvas!.height);
 
-    // draw the background with slight transparency for the game board
-    drawRectangle(
-      context!,
-      { x: 0, y: 0, a: 600, b: 600, color: "#00808057" },
-      RectangleType.FILL,
-    );
+		// draw the background with slight transparency for the game board
+		drawRectangle(
+			context!,
+			{ x: 0, y: 0, a: 600, b: 600, color: "#00808057" },
+			RectangleType.FILL,
+		);
 
-    // draw the items
-    readyItems
-      .filter((x) => !x.isCollected)
-      .forEach((x) => {
-        drawCircle(context!, CircleType.FILL, {
-          x: x.x,
-          y: x.y,
-          r: x.r,
-          color: x.color,
-        });
-      });
+		// draw the items
+		readyItems
+			.filter((x) => !x.isCollected)
+			.forEach((x) => {
+				drawCircle(context!, CircleType.FILL, {
+					x: x.x,
+					y: x.y,
+					r: x.r,
+					color: x.color,
+				});
+			});
 
-    // draw the player in the bottom right corner
-    drawCircle(context!, CircleType.FILL, {
-      x: playerX,
-      y: playerY,
-      r: playerRadius,
-      color: player.color,
-    });
+		// draw the player in the bottom right corner
+		drawCircle(context!, CircleType.FILL, {
+			x: playerX,
+			y: playerY,
+			r: playerRadius,
+			color: player.color,
+		});
 
-    const loop = () => {
-      // draw the game board
-      drawRectangle(
-        context!,
-        { x: 50, y: 50, a: 500, b: 500, color: "rgb(100, 100, 0)" },
-        RectangleType.STROKE,
-      );
+		const loop = () => {
+			// draw the game board
+			drawRectangle(
+				context!,
+				{ x: 0, y: 0, a: 600, b: 600, color: "rgb(100, 100, 0)" },
+				RectangleType.STROKE,
+			);
 
-      // draw the player in the bottom right corner
-      drawCircle(context!, CircleType.FILL, {
-        x: playerX,
-        y: playerY,
-        r: playerRadius,
-        color: player.color,
-      });
+			// draw the player in the bottom right corner
+			drawCircle(context!, CircleType.FILL, {
+				x: playerX,
+				y: playerY,
+				r: playerRadius,
+				color: player.color,
+			});
 
-      // implement logic to check if on the current position there are items and if the player collides with them
-      checkPlayerItemsCollision();
+			// implement logic to check if on the current position there are items and if the player collides with them
+			checkPlayerItemsCollision();
 
-      requestAnimationFrame(loop);
-    };
-    loop();
-  };
+			requestAnimationFrame(loop);
+		};
+		loop();
+	};
 
-  const handleKeyPress = (e: KeyboardEvent) => {
-    switch (e.key) {
-      case "w":
-        playerY -= 5 + playerSpeed;
-        break;
-      case "a":
-        playerX -= 5 + playerSpeed;
-        break;
-      case "s":
-        playerY += 5 + playerSpeed;
-        break;
-      case "d":
-        playerX += 5 + playerSpeed;
-        break;
-      default:
-        return;
-    }
+	const handleKeyPress = (e: KeyboardEvent) => {
+		switch (e.key) {
+			case "w":
+				//up
+				if (playerY - playerRadius - 5 - playerSpeed < 0) return;
+				playerY -= 5 + playerSpeed;
+				break;
+			case "a":
+				//left
+				if (playerX - playerRadius - 5 - playerSpeed < 0) return;
+				playerX -= 5 + playerSpeed;
+				break;
+			case "s":
+				//down
+				if (playerY + playerRadius + 5 + playerSpeed > 600) return;
+				playerY += 5 + playerSpeed;
+				break;
+			case "d":
+				//right
+				if (playerX + playerRadius + 5 + playerSpeed > 600) return;
+				playerX += 5 + playerSpeed;
+				break;
+			default:
+				return;
+		}
 
-    renderGame();
-  };
+		renderGame();
+	};
 
-  useEffect(() => {
-    if (canvasRef && canvasRef.current && !context) {
-      const canvas = canvasRef.current;
-      canvas.width = 600;
-      canvas.height = 600;
-      context = canvasRef.current.getContext("2d");
-    }
-    renderGame();
+	useEffect(() => {
+		if (canvasRef && canvasRef.current && !context) {
+			const canvas = canvasRef.current;
+			canvas.width = 600;
+			canvas.height = 600;
+			context = canvasRef.current.getContext("2d");
+		}
+		renderGame();
 
-    window.addEventListener("keydown", handleKeyPress);
+		window.addEventListener("keydown", handleKeyPress);
 
-    return () => window.removeEventListener("keydown", handleKeyPress);
-  }, []);
+		return () => window.removeEventListener("keydown", handleKeyPress);
+	}, []);
 
-  return <canvas ref={canvasRef} />;
+	return <canvas ref={canvasRef} />;
 };
 
 export default CanvasElement;
