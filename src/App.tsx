f085b70d3b5bcbda5c4d2747b4b9fc1f7adf1787
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-06-05 22:42:35 +0300

    implemented a warping circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import "./App.css";
 import CanvasElement from "./components/CanvasElement";
+import { useRef, useEffect } from "react";
+import { type Circle, CircleType } from "./types/CanvasTypes";
+import { drawCircle } from "./utilities/CanvasUtils";
 
 function App() {
   const player = {
@@ -44,12 +47,61 @@ function App() {
       playerSpeedIncrement: 3,
     },
   ];
+  {
+    /* <> */
+  }
+  {
+    /*   <CanvasElement player={player} items={items} /> */
+  }
+  {
+    /* </> */
+  }
 
-  return (
-    <>
-      <CanvasElement player={player} items={items} />
-    </>
-  );
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = ref.current;
+    if (canvas) {
+      const circle: Circle = {
+        x: 590,
+        y: 590,
+        r: 10,
+        color: "rgb(0,255,255)",
+      };
+
+      const drawCanvas = () => {
+        const context2d = canvas.getContext("2d");
+        context2d.fillStyle = "rgb(0,0,0)"; //research why optional chaining asign is not working context2d?.fillStyle
+        context2d.fillRect(0, 0, 600, 600);
+
+        drawCircle(context2d, CircleType.FILL, circle);
+      };
+      // drawCanvas();
+
+      const moveCircle = () => {
+        if (circle.x - 0.3 < 0) {
+          circle.x = 590;
+        } else {
+          circle.x -= 0.3;
+        }
+
+        if (circle.y + 1 > 600) {
+          circle.y = 0;
+        } else {
+          circle.y += 1;
+        }
+
+        drawCanvas();
+
+      };
+
+      // moveCircle();
+       setInterval(() => {
+         moveCircle();
+       }, 1000 / 180);
+    }
+  }, []);
+  return <canvas ref={ref} width={600} height={600} />;
 }
 
 export default App;
